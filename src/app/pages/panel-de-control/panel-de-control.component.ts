328388e391000f3c027ce0c299c55ba25ea9d8c2
stiven1920 <jhersson.jojoa.6265@eam.edu.co>
2023-04-01 04:18:56 -0500

    cambios al servicio de calcular

--- src/app/pages/panel-de-control/panel-de-control.component.ts
+++ src/app/pages/panel-de-control/panel-de-control.component.ts
@@ -8,24 +8,59 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class PanelDeControlComponent implements OnInit {
   userLogged: any;
+  categoria!: number;
+  horasExtras!: number;
   constructor( private authService: AuthService) {
     this.authService.getUserLogged().subscribe(res => {
       this.userLogged = res;
     })
   }
-sueldo ={
-  categoria : 0,
-  sueldo : 0,
-  horasExtras :  0
-}
+
   ngOnInit(): void {
 
   }
 
-  CalcularSueldo(){
-    console.log("CalcularSueldo", this.CalcularSueldo);
-    const {categoria, horasExtras} = this.sueldo;
-    this.authService.CalcularSueldo(categoria, horasExtras);
+
+
+  CalcularSueldo() {
+    try {
+      let elExtra;
+      if (this.categoria >= 1) {
+        if (this.categoria <= 5) { 
+          if ( this.categoria == 1 && this.horasExtras > 40 ||  this.horasExtras == 40) {
+            elExtra = 8000;
+             const resultado = ((elExtra * 40) + (( this.horasExtras - 40) * 0.25) * elExtra);
+            alert("El sueldo del empleado es: " + resultado)
+            console.log("El sueldo del empleado es: " + resultado);
+          } else if (this. categoria == 2 &&  this.horasExtras > 40 ||  this.horasExtras == 40) {
+            elExtra = 12000;
+            const resultado = ((elExtra * 40) + (( this.horasExtras - 40) * 0.25) * elExtra);
+            console.log("El sueldo del empleado es: " + resultado);
+          } else if ( this.categoria == 3 &&  this.horasExtras > 40 ||  this.horasExtras == 40) {
+            elExtra = 18000;
+            const resultado = ((elExtra * 40) + (( this.horasExtras - 40) * 0.25) * elExtra);
+            console.log("El sueldo del empleado es: " + resultado);
+          } else if ( this.categoria == 4 &&  this.horasExtras > 40 ||  this.horasExtras == 40) {
+            elExtra = 25000;
+            const resultado = ((elExtra * 40) + (( this.horasExtras - 40) * 0.25) * elExtra);
+            console.log("El sueldo del empleado es: " + resultado);
+          } else if ( this.categoria == 5 &&  this.horasExtras > 40 ||  this.horasExtras == 40) {
+            elExtra = 32000;
+            const resultado = ((elExtra * 40) + (( this.horasExtras - 40) * 0.25) * elExtra);
+            console.log("El sueldo del empleado es: " + resultado);
+          }
+        } else {
+          alert("no hay categorias mayores a 5");
+          console.log("no hay categorias mayores a 5");
+        }
+      } else {
+        alert("no hay categorias mayores a 5");
+        console.log("no hay categorias menores a 1");
+      }
+    } catch (error) {
+
+
+    }
   }
   
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -46,46 +46,6 @@ export class AuthService {
     this.afauth.signOut();
   }
 
-  CalcularSueldo(categoria:number, horasExtras:number) {
-    try {
-      let elExtra;
-      if (categoria >= 1) {
-        if (categoria <= 5) { 
-          if ( categoria == 1 && horasExtras > 40 ||  horasExtras == 40) {
-            elExtra = 8000;
-             const resultado = ((elExtra * 40) + (( horasExtras - 40) * 0.25) * elExtra);
-            alert("El sueldo del empleado es: " + resultado)
-            console.log("El sueldo del empleado es: " + resultado);
-          } else if ( categoria == 2 &&  horasExtras > 40 ||  horasExtras == 40) {
-            elExtra = 12000;
-            const resultado = ((elExtra * 40) + (( horasExtras - 40) * 0.25) * elExtra);
-            console.log("El sueldo del empleado es: " + resultado);
-          } else if ( categoria == 3 &&  horasExtras > 40 ||  horasExtras == 40) {
-            elExtra = 18000;
-            const resultado = ((elExtra * 40) + (( horasExtras - 40) * 0.25) * elExtra);
-            console.log("El sueldo del empleado es: " + resultado);
-          } else if ( categoria == 4 &&  horasExtras > 40 ||  horasExtras == 40) {
-            elExtra = 25000;
-            const resultado = ((elExtra * 40) + (( horasExtras - 40) * 0.25) * elExtra);
-            console.log("El sueldo del empleado es: " + resultado);
-          } else if ( categoria == 5 &&  horasExtras > 40 ||  horasExtras == 40) {
-            elExtra = 32000;
-            const resultado = ((elExtra * 40) + (( horasExtras - 40) * 0.25) * elExtra);
-            console.log("El sueldo del empleado es: " + resultado);
-          }
-        } else {
-          alert("no hay categorias mayores a 5");
-          console.log("no hay categorias mayores a 5");
-        }
-      } else {
-        alert("no hay categorias mayores a 5");
-        console.log("no hay categorias menores a 1");
-      }
-    } catch (error) {
-
-
-    }
-  }
 
 
 }
